50963c60d4b342c0e593122ec8a92f8408a5e8bd
Dillon Marinez <dimarinez@yahoo.com>
2016-05-18 11:36:24 -0700

    slider

--- client/main.js
+++ client/main.js
@@ -3,3 +3,10 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+Template.main.helpers({
+  'intro': routeName => Router.current().route.getName() === routeName ? 'intro' : ''
+  ,
+  transition: () => (from, to, element) => to.template === "intro" ? 'left-to-right' :  'right-to-left',
+
+});
+
